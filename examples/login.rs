db58739d1e5d231f2834891d4a7d4c729cd808e2
SyedAhkam <smahkam57@gmail.com>
2021-06-05 01:16:30 +0530

    oops

--- examples/login.rs
+++ examples/login.rs
@@ -8,7 +8,7 @@ async fn main() -> RustyCordResult<()> {
     let mut client = Client::new();
 
     client
-        .run("NzY4MjA0MzkxNTU1Mzk5NzIy.X49EFw.2Bk5AFeVsRDWXh-14Y8Wamaszbk")
+        .run("TOKEN")
         .await?;
 
     println!("{:#?}", client);
