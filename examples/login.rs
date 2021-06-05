c8243e5d5bed938ba8cd0d87f8102eef9545353f
SyedAhkam <smahkam57@gmail.com>
2021-06-05 21:14:15 +0530

    Better structure

--- examples/login.rs
+++ examples/login.rs
@@ -1,17 +1,19 @@
 use rustycord::{
-    Client,
-    RustyCordResult
+    Result,
+    client::Error
 };
 
 #[tokio::main]
-async fn main() -> RustyCordResult<()> {
-    let mut client = Client::new();
+async fn main() -> Result<()> {
+    // let mut client = Client::new();
+//
+    // client
+        // .run("NzY4MjA0MzkxNTU1Mzk5NzIy.X49EFw.2Bk5AFeVsRDWXh-14Y8Wamaszbk")
+        // .await?;
+//
+    // println!("{:#?}", client);
 
-    client
-        .run("NzY4MjA0MzkxNTU1Mzk5NzIy.X49EFw.2Bk5AFeVsRDWXh-14Y8Wamaszbk")
-        .await?;
-
-    println!("{:#?}", client);
+    println!("ok");
 
     Ok(())
 }
---
+++ src/http/mod.rs
@@ -0,0 +1,8 @@
+// Public modules
+pub mod error;
+pub mod http;
+
+// Re-exports
+pub use http::HTTPClient;
+
+pub use error::Error;
